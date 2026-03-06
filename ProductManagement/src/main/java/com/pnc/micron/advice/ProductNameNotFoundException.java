4da387121c7306e60c2eb15a4cf599834cea511d
Lokesh Jangam <lokesh12081990@gmail.com>
2026-03-06 14:56:50 +0530

    Delete ProductManagement directory

--- ProductManagement/src/main/java/com/pnc/micron/advice/ProductNameNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.pnc.micron.advice;
-
-public class ProductNameNotFoundException extends RuntimeException {
-	public ProductNameNotFoundException(String name) {
-		super(name);
-	}
-
-}
--- ProductManagement/src/main/java/com/pnc/micron/advice/ProductPriceException.java
+++
@@ -1,8 +0,0 @@
-package com.pnc.micron.advice;
-
-public class ProductPriceException extends RuntimeException{
-	public ProductPriceException(String name){
-		super(name);
-	}
-
-}
